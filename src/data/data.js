f8c8f4a5995ed99e380ef4289b4dda29b4da7911
manish kumar <mk7219630@gmail.com>
2026-09-16 17:32:46 +0530

    new pdf added

--- src/data/data.js
+++ src/data/data.js
@@ -1094,6 +1094,8 @@ export const products = [
     "stadiums",
   ],
 
+   pdf: "/smd.pdf",
+
   metaTitle:
     "NVS P4 SMD Gold Wired LED Display | High-Resolution Outdoor LED Video Wall | Newtech Video Systems",
 
@@ -3127,6 +3129,9 @@ export const products = [
     catId: "accessories-and-controllers",
     catName: "Accessories & Controllers",
     img: "/nvs601.webp",
+     pdf: "/NVS-600(1.01).pdf",
+
+
 
     images: [
       "/nvs601.webp",
@@ -3254,6 +3259,7 @@ export const products = [
   {
   name: "NVS 600 (1.02)",
   id: "nvs-600-1-02",
+   pdf: "/NVS600(1.02).pdf",
 
   applicationIds: [
     "corporate",
@@ -3807,6 +3813,7 @@ export const products = [
   {
   name: "NVS CB2 (1.01)",
   id: "nvs-cb2-1-01",
+   pdf: "/nvscb(1.01).pdf",
 
   applicationIds: [
     "corporate",
